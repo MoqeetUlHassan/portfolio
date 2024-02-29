b44f84a3d3c0838d55f735528037358f6c0f7a23
Moqeet <moqeet1998@gmail.com>
2024-03-01 00:14:28 +0500

    3rd sec

--- portfolio/src/components/Experience.jsx
+++ portfolio/src/components/Experience.jsx
@@ -1,13 +1,82 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const data = ['Development', 'Three js', 'Laravel', 'Node', 'Flutter'];
+
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+`;
+const Container = styled.div`
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const ListItem = styled.li`
+  font-size: 90px;
+  font-weight: bold;
+  color: transparent;
+  -webkit-text-stroke: 1px white;
+  position: relative;
+
+  ::after {
+    content: '${(props) => props.text}';
+    position: absolute;
+    top: 0;
+    left: 0;
+    color: #0070d3;
+    width: 0;
+    overflow: hidden;
+  }
+
+  &:hover {
+    ::after {
+      animation: moveText 0.5 linear;
+      cursor: pointer;
+      @keyframes MoveTExt {
+        to {
+          width: 100%;
+        }
+      }
+    }
+  }
+`;
+
+const Right = styled.div`
+  flex: 1;
 `;
 
 const Experience = () => {
-  return <Section>EXP</Section>;
+  return (
+    <Section>
+      <Container>
+        <Left>
+          <List>
+            {data.map((item) => (
+              <ListItem key={item} text={item}>
+                {item}
+              </ListItem>
+            ))}
+          </List>
+        </Left>
+        <Right></Right>
+      </Container>
+    </Section>
+  );
 };
 
 export default Experience;
--- portfolio/src/components/Person.jsx
+++ portfolio/src/components/Person.jsx
@@ -4,10 +4,79 @@ import styled from 'styled-components';
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Left3d = styled.div`
+  flex: 1;
+`;
+
+const RightDesc = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20;
+`;
+const Title = styled.h1``;
+
+const Details = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Line = styled.img`
+  height: 5px;
+`;
+
+const Subtext = styled.h2`
+  color: aqua;
+`;
+
+const Desc = styled.p`
+  font-size: 24px;
+  color: lightgrey;
+`;
+
+const Button = styled.button`
+  background-color: aqua;
+  color: black;
+  font-weight: 500;
+  width: 100px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
 `;
 
 const Person = () => {
-  return <Section>Profile</Section>;
+  return (
+    <Section>
+      <Container>
+        <Left3d></Left3d>
+        <RightDesc>
+          <Title>Think outside the box.</Title>
+          <Details>
+            <Line src="./img/line.png" />
+            <Subtext>hobbies</Subtext>
+          </Details>
+          <Desc>Developer who likes innovations</Desc>
+          <Button>Try work?</Button>
+        </RightDesc>
+      </Container>
+    </Section>
+  );
 };
 
 export default Person;
